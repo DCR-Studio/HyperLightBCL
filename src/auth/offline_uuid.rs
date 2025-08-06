b7113c73e246aa08bc2f4856debbb9b18f8c52b5
TNTyep520 <3345562481@qq.com>
2025-08-06 22:12:09 +0800

    修复输入中文字符时出现退格错误，完善 Ofline_UUID.rs 部分代码

--- src/auth/offline_uuid.rs
+++ src/auth/offline_uuid.rs
@@ -1,21 +1,27 @@
 use md5;
-use std::io::{self, Write};
+use regex::Regex;
+use rustyline::Editor;
+use rustyline::history::DefaultHistory;
 
 fn main() {
-    print!("请输入玩家名称: ");
-    io::stdout().flush().unwrap();
-    
-    let mut name = String::new();
-    io::stdin().read_line(&mut name).unwrap();
-    let name = name.trim();
+    let mut rl = Editor::<(), DefaultHistory>::new().unwrap();
+    let prompt = "请输入玩家名称: ";
+
+    let name = rl.readline(prompt).unwrap_or_default().trim().to_string();
 
     if name.is_empty() {
         println!("\x1b[31mError: 你输入的玩家名称不能为空\x1b[0m");
         return;
     }
 
+    let re = Regex::new(r"^[A-Za-z0-9_]+$").unwrap();
+    if !re.is_match(&name) {
+        println!("\x1b[31mError: 玩家名称只允许使用字母、数字及下划线。\x1b[0m");
+        return;
+    }
+
     println!("\nPlayer Name: {}", name);
-    
+
     let mut bytes = md5::compute(format!("OfflinePlayer:{}", name)).0;
     bytes[6] = (bytes[6] & 0x0F) | 0x30;
     bytes[8] = (bytes[8] & 0x3F) | 0x80;
